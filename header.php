88dcb7203b94098682021848911680f9f0452043
Stockholm <stockholm@Stockholm.local>
2020-01-06 19:36:20 +0000

    Add draft of navbar

--- header.php
+++ header.php
@@ -14,7 +14,8 @@
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="css/main.css">
-
+  <!-- Fontawesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
   <meta name="theme-color" content="#fafafa">
 </head>
 
@@ -25,5 +26,32 @@
 
 
 <header>
+  <div class="banner">
+    <img src="https://source.unsplash.com/random/1200x200" alt="Hyatt Privé Lense – Banner">
+    <div class="banner__overlay">
+      <!-- Logos here -->
+    </div>
+  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-bars"></i> MENU
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link" href="#" id="navbarDropdownMenuLink" role="button">
+            <i class="fas fa-arrow-right"></i> NEXT
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
 
 </header>
