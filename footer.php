287aa8898668e02ee0fc1299e24670057971d955
Julian Steinmann <steinmann.julian@gmail.com>
2020-01-07 19:29:12 +0000

    Add footer from hyatt site

--- footer.php
+++ footer.php
@@ -1,6 +1,27 @@
 
 </div> <!-- page-wrapper -->
-<footer>Footer here</footer>
+
+<footer class="ui-footer">
+  <section class="helpline">
+    <h4>Need Reservation Support?</h4>
+    <button type="button" class="call-us-button">Call us</button>
+  </section>
+  <div data-bookends="legal-footer">
+    <div class="hbe hbe-legal hbe-legal_theme-carbon" lang="en-US">
+      <div class="hbe-container_1280">
+        <ul>
+          <li><a href="https://www.hyatt.com/en-US/info/privacy-policy">Privacy Policy</a></li>
+          <li><a href="https://help.hyatt.com/en/hyatt-terms.html">Terms &amp; Conditions</a></li>
+          <li><a href="https://help.hyatt.com/en/hyatt-terms/cookie-statement.html">Cookie Statement</a></li>
+          <li><a href="https://about.hyatt.com/en/security-safety.html">Security &amp; Safety</a></li>
+          <li><a href="https://about.hyatt.com/content/dam/HyattStories/thrive/Hyatt-Global-Human-Rights-Statement-Modern-Day-Slavery-020117.pdf">Supply Chain Statement</a></li>
+          <li><a href="https://www.hyatt.com/forms/do-not-sell">Do Not Sell My Personal Information</a></li>
+          <li><span>© 2019 Hyatt Corporation</span></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
 
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
