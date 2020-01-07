b9a6cf687e21dc86a35cc3bf231bf1c844b0e746
Julian Steinmann <steinmann.julian@gmail.com>
2020-01-07 19:28:43 +0000

    Add content of home page

--- index.php
+++ index.php
@@ -2,11 +2,82 @@
 <main class="page--index">
 
 
+<main class="page--index">
   <div class="container">
-    <div class="card">
-      <h1>HOME</h1>
-      <p>Hello world! This is HTML5 Boilerplate.</p>
+
+    <div class="bg-image bg-image--top-left">
+      <img src="img/PH_P048_Resort_Celebration.jpg" alt="">
     </div>
+    <div class="card card--bottom-right">
+      <h5>INTRODUCTION</h5>
+      <h2>Welcome to Hyatt Privé Lens</h2>
+      <p>
+        Designed For You, By You<br>
+        In This Volume
+      </p>
+      <a href="#">GET STARTED</a>
+    </div>
+
+    <div class="bg-image bg-image--top-right">
+      <img src="img/NYCPH_P536_Carnegie_Suite.jpg" alt="">
+    </div>
+    <div class="card card--bottom-left">
+      <h5>A BRAND STORY</h5>
+      <h2>Celebrating 40 Years of Park Hyatt</h2>
+      <p>
+        The Vision<br>
+        Iconic Art, Architecture & Design<br>
+        Masters of Food & Wine<br>
+        What’s Next</p>
+      <a href="#">READ MORE</a>
+    </div>
+
+    <div class="bg-image bg-image--top-left">
+      <img src="img/DPSAU_P001_Villa_Exterior.jpg" alt="">
+    </div>
+    <div class="card card--bottom-right">
+      <h5>AN EXPERIENCE STORY</h5>
+      <h2>Alila & Sustainability</h2>
+      <p>
+        For the Love of Earth—and Coconuts—in Bali<br>
+        Caring For the Island of the Gods<br>
+        Building Greener
+      </p>
+      <a href="#">EXPERIENCE MORE</a>
+    </div>
+
+    <div class="bg-image bg-image--bottom-right">
+      <img src="img/LHRUB_P002_Exterior_Evening.jpg" alt="">
+    </div>
+    <div class="card card--bottom-left">
+      <h5>NEW OPENINGS STORY</h5>
+      <h2>Six New and Noteworthy Hotels</h2>
+      <p>
+        Andaz Dubai The Palm<br>
+        Andaz Seoul Gangnam<br>
+        Great Scotland Yard Hotel<br>
+        Thompson Washington D.C.<br>
+        Park Hyatt Auckland<br>
+        Park Hyatt Kyoto
+      </p>
+      <a href="#">EXPLORE THE WORLD</a>
+    </div>
+
+    <div class="bg-image bg-image--left-bottom">
+      <img src="img/shutterstock_1187224183.jpg" alt="">
+    </div>
+    <div class="card card--top-right">
+      <h5>NEW OPENINGS STORY</h5>
+      <h2>Hit the Slopes at these Top Hyatt Ski Resorts</h2>
+      <p>
+        Ski-in, ski-out, and brave the Back Bowls in Vail<br>
+        An upscale mountain retreat tucked beside Lake Tahoe<br>
+        High-altitude Alpine adventures await at La Rosière<br>
+        Perfect powder and Michelin-starred dining in Niseko
+      </p>
+      <a href="#">EXPERIENCE MORE</a>
+    </div>
+
   </div>
 </main>
 
