b7b54652f108e0e989779b217237879c9f52ebb1
Julian Steinmann <steinmann.julian@gmail.com>
2020-01-08 19:33:28 +0000

    Add content of all pages

--- celebrate_park_hyatt.php
+++ celebrate_park_hyatt.php
@@ -4,11 +4,57 @@ $page_title = "Welcome - Hyatt Privé Lense";
 $next_page = "alila_sustainability.php";
 include "header.php" ?>
 
+<picture>
+  <source media="(min-width: 1500px)" srcset="img/celebrate_nycph_p616_manhattan_sky_suite_master_bedroom_xl.jpg">
+  <source media="(min-width: 1200px)" srcset="img/celebrate_nycph_p616_manhattan_sky_suite_master_bedroom_l.jpg">
+  <source media="(min-width: 900px)" srcset="img/celebrate_nycph_p616_manhattan_sky_suite_master_bedroom_m.jpg">
+  <img src="img/celebrate_nycph_p616_manhattan_sky_suite_master_bedroom_s.jpg" class="banner__img" width="100%" alt="Hyatt Privé Lense – Banner">
+</picture>
+<main class="page--content">
+  <div class="container container--content">
 
-<main class="page--index">
-  <div class="container">
+  <div class="card--content ">
 
-  Celebrate
+    <section class="content-section">
+      <h5>VOLUME ONE</h5>
+      <h1>Celebrating 40 Years of Park Hyatt</h1>
+      <p>We are delighted to celebrate 40 years of our Park Hyatt brand – four decades of exceptional service, renowned art and architecture, rare culinary experiences, and unexpected pleasures. Discerning travelers know that true luxury is personal—whether it’s an animated discussion of modern art over an aged, single-malt scotch or an intimate lesson in delicate Japanese calligraphy. Park Hyatt hotels specialize in these sophisticated connections.</p>
+    </section>
+
+    <div class="bg-image--content bg-image--content--top-right">
+      <img src="img/celebrate_jaypritzner.jpg" alt="">
+      <a href="https://videos.hyatt.com/park_hyatt_Pritzker.mp4" target="_blank"class="text-center">Watch the interview</a>
+    </div>
+    <section class="content-section content-section--left bg-image--content--below-right">
+      <h2>The Vision</h2>
+      <p>Philanthropist and entrepreneur Jay Pritzker founded Park Hyatt in 1979. His vision for the brand was deeply personal—and inspired by his love of extraordinary food and wine, culture, and the arts. Pritzker set out to create a “home away from home” that captured the personalized, intuitive service and understated elegance of his favorite small European hotels.</p>
+    </section>
+
+    <section class="content-section content-section--left">
+      <h2>Iconic Art, Architecture & Design</h2>
+      <p>World-class design has remained at the heart of the Park Hyatt brand, from the legendary Chicago property that started it all to development spanning from Marrakech to Mexico City. Each hotel draws from its location to create a singularly unique experience through stunning architecture, fine furnishings, and expertly curated art collections.</p>
+      <p>Long before Lost In Translation brought the hotel film fame, Park Hyatt Tokyo debuted at the top of a 52-story Shinjuku tower and redefined the meaning of luxury hospitality in Japan. In the Austrian capital, a century-old bank transformed into a masterpiece of opulent, contemporary design at Park Hyatt Vienna.</p>
+      <p>Across the pond in Midtown Manhattan, a multi-million dollar art collection set a new standard of luxury at Park Hyatt New York. Here, the Manhattan Sky Suite, New York City’s highest hotel suite has recently debuted, overlooking unmatched views of Central Park and the city skyline from the 59th floor. In true Park Hyatt fashion, the Manhattan Sky Suite offers worldly guests an elevated luxury NYC experience, with complimentary helicopter airport transfer, dedicated butler service, a personal chef, and much more.</p>
+  </section>
+
+  <div class="bg-image--content bg-image--content--top-left">
+    <img src="img/celebrate_mfw_p173_appetizer_prep.jpg" alt="">
+  </div>
+  <section class="content-section content-section--right">
+    <h2>Masters of Food & Wine</h2>
+    <p>Furthering Jay Pritzker’s original vision for the brand, Park Hyatt launched its Masters of Food & Wine program in 2003. The series showcases Park Hyatt chefs, sommeliers, and specialists through intimate, immersive culinary and beverage events all over the world.</p>
+    <p>Past Masters of Food & Wine events include a twilight fishing excursion and traditional cooking class at Park Hyatt Maldives Hadahaa and a visit to local Argentine olive groves with an olive oil tasting at Park Hyatt Mendoza. No two Masters of Food & Wine events are alike. Coming soon to Park Hyatt Zurich, Executive Chef Frank Widmer will guide guests through a cozy Swiss chalet dinner, grilling a carefully selected menu of seasonal favorites, paired with LVMH wines.</p>
+</section>
+
+<div class="bg-image--content bg-image--content--top-right">
+  <img src="img/celebrate_global_p047_lifestyle_entrance_couple_walking.jpg" alt="">
+</div>
+<section class="content-section content-section--middle">
+  <h2>What’s Next</h2>
+  <p>Ever attentive to the desires of discerning global travelers, 2020 will see exciting additions to the brand’s already-attractive portfolio.</p>
+  <p>Park Hyatt Toronto reopens in 2020 after a stunning, top-to-bottom renovation. The brand continues its tradition of unparalleled luxury in Japan, with the additions of Park Hyatt Niseko Hanazono and Park Hyatt Kyoto. Park Hyatt Doha debuts this December and the opening of Park Hyatt Auckland marks the brand’s entry into New Zealand in early 2020.</p>
+  <p>As the definition of luxury evolves, Park Hyatt will remain a leader in rare and renowned experiences. We thank you for joining us in celebrating the 40th anniversary of this iconic brand.</p>
+</section>
   </div>
 </main>
 
--- new_hotels.php
+++ new_hotels.php
@@ -4,11 +4,63 @@ $page_title = "Welcome - Hyatt Privé Lense";
 $next_page = "hyatt_ski_resorts.php";
 include "header.php" ?>
 
+<picture>
+  <source media="(min-width: 1500px)" srcset="img/new_hotels_lhrub_p002_exterior_evening_xl.jpg">
+  <source media="(min-width: 1200px)" srcset="img/new_hotels_lhrub_p002_exterior_evening_l.jpg">
+  <source media="(min-width: 900px)" srcset="img/new_hotels_lhrub_p002_exterior_evening_m.jpg">
+  <img src="img/new_hotels_lhrub_p002_exterior_evening_s.jpg" class="banner__img" width="100%" alt="Hyatt Privé Lense – Banner">
+</picture>
+<main class="page--content">
+  <div class="container container--content">
 
-<main class="page--index">
-  <div class="container">
+  <div class="card--content ">
 
-  New Hotels
+    <section class="content-section">
+      <h5>NEW OPENINGS STORY</h5>
+      <h1>Now Booking: Six New and Noteworthy Hyatt Hotels Opening Worldwide</h1>
+      <p>What do cosmopolitan capitals, trendy waterfront neighborhoods, and the world’s most iconic man-made island have in common? All are home to some of Hyatt’s most exciting new hotels worldwide. Brands will make their debut soon in beloved destinations like Dubai and London, while Hyatt’s highly-anticipated return to New Zealand opens in Auckland. Get to know a few of the newest additions to Hyatt’s global portfolio below.</p>
+    </section>
+    <div class="bg-image--content bg-image--content--top-left">
+      <img src="img/new_hotels_selaz_p001_king_guestroom.jpg" alt="">
+    </div>
+    <section class="content-section content-section--right bg-image--content--below-left">
+      <h2>Andaz Dubai The Palm</h2>
+      <p>See those gleaming towers rising over The Palm Jumeirah? Andaz has arrived in Dubai at last. Located on the iconic, tree-shaped Palm Jumeirah island, Andaz Dubai The Palm channels the colors and energy of Arabia throughout its 217 guest rooms and 115 serviced apartments. Travelers immerse themselves in Middle Eastern-inspired flavors and spices at one of five distinct dining experiences while reveling in pop-up art exhibitions featuring local talent. On the 14th floor, true tranquility awaits at the Ora boutique spa. Andaz Dubai The Palm is accepting bookings from December 1, 2019.</p>
+    </section>
+
+    <div class="bg-image--content bg-image--content--bottom-left">
+      <img src="img/new_hotels_dxbct_p037_standard_king_guestroom.jpg" alt="">
+    </div>
+    <section class="content-section content-section--middle">
+      <h2>Andaz Seoul Gangnam</h2>
+      <p>Debuting earlier this year, Andaz Seoul Gangnam marks the fifth Hyatt-branded hotel in South Korea and the first Andaz. The hotel is right at home within the Gangnam district’s Apgujeong neighborhood, known for its high-end shopping and K-Pop studios. The style and sounds of this vibrant corner of the capital are celebrated in every design detail, drawing inspiration from bojagi, traditional Korean patchwork weaving art. Meanwhile, Seoul’s immersive alleyway eateries are elevated to surprising heights at Jogakbo, an exciting multi-concept space for dining and drinking. Andaz Seoul Gangnam is now open.</p>
+    </section>
+
+    <section class="content-section content-section--right">
+      <h2>Great Scotland Yard</h2>
+      <p>The United Kingdom is set to welcome the first hotel to open under The Unbound Collection By Hyatt brand. Great Scotland Yard is ready for a “smashing” debut in London. The former headquarters of the Metropolitan Police is now a one-of-a-kind luxury hotel, where a storied past is etched onto nearly every inch of the new, bespoke design. There are 153 stylish bedrooms and 11 suites, including a connected five-story townhouse, offering a uniquely intimate stay for the most discerning travelers. Secret doors conceal more hidden rooms, while celebrity chef Robin Gill brings a taste of the English countryside to the beating heart of Britain’s capital. Great Scotland Yard is now open.</p>
+    </section>
+
+    <div class="bg-image--content bg-image--content--top-right">
+      <img src="img/new_hotels_iadth_r007_exterior.jpg" alt="">
+    </div>
+    <section class="content-section content-section--middle">
+      <h2>Thompson Washington D.C.</h2>
+      <p>Designed to be a gathering place for Washingtonians and visitors alike, Thompson Washington D.C. is set to open early in early 2020. The 225-room lifestyle hotel is steps from the Anacostia River and poised to become the social anchor of its trendy Navy Yards neighborhood. A rooftop bar and multiple restaurants on-site invite spirited conversation, pairing perfectly with a stroll along the Riverwalk Trail or an exciting baseball game at nearby Nationals Park. Its design pays homage to the industrial and maritime history of the area, featuring elements of naval style as well as copper and metalwork. Thompson Washington D.C. is accepting bookings from March 1, 2020.</p>
+    </section>
+
+    <section class="content-section content-section--left bg-image--content--below-right">
+      <h2>Park Hyatt Auckland</h2>
+      <p>Kia ora, New Zealand! Park Hyatt Auckland opens its doors early next year, marking Hyatt’s return to New Zealand. Poised on the water’s edge in stylish Wynyard Quarter, this modern hotel offers sweeping views over Waitematā Harbour—an enviable position ahead of the 2021 America’s Cup. Also overlooking the water is a 25-meter infinity pool, ideal for a refreshing dip after a luxurious spa treatment or afternoon spent lounging on the sun deck. Residential-inspired guest rooms and suites are elegantly appointed in rich woods, marble, and brass finishes, exuding luxury around every corner. In true Park Hyatt fashion, an elevated culinary experience awaits, featuring premium produce and in-season ingredients from around New Zealand. Park Hyatt Auckland is accepting bookings from March 1, 2020.</p>
+    </section>
+
+    <div class="bg-image--content bg-image--content--bottom-left">
+      <img src="img/new_hotels_itmph_p105_hotel_exterior.jpg" alt="">
+    </div>
+    <section class="content-section content-section--right bg-image--content--below-left">
+      <h2>Park Hyatt Kyoto</h2>
+      <p>Combining heritage guesthouse architecture with interiors by renowned hospitality designer Tony Chi, the newly-opened Park Hyatt Kyoto is poised to welcome guests with unrivaled omotenashi. This traditional Japanese hospitality philosophy presents luxury in every detail. Nestled beside the Kōdaiji Temple within the historic Higashiyama district, Park Hyatt Kyoto is an intimate hillside retreat designed in harmony with its peaceful surroundings. This serene setting is ideal for enjoying the region’s splendid seasons—including magnificent cherry blossoms—or for travelers planning a deeper exploration of Japan ahead of the 2020 Tokyo Olympics. Park Hyatt Kyoto is now open.</p>
+    </section>
   </div>
 </main>
 
