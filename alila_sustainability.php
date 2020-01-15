4d65e401b14b092b74c587bee65f437d646eb422
Julian Steinmann <steinmann.julian@gmail.com>
2020-01-15 19:39:42 +0000

    Adding Links to properties

--- alila_sustainability.php
+++ alila_sustainability.php
@@ -18,7 +18,7 @@ include "template_parts/header.php" ?>
     <section class="content-section">
       <h5>AN EXPERIENCE STORY</h5>
       <h1>For the Love of Earth—and Coconuts—in Bali</h1>
-      <p>In Bahasa Indonesia, cinta Alam means “Love of Earth.” This concept is at the heart of everything at <strong>Alila Seminyak</strong>, where glass water bottles become sleek drinkware and discarded coconuts find new life as organic planters. As global leaders in sustainable tourism, Alila Hotels & Resorts launched the “Zero Waste to Landfill” project in August 2016, challenging its flagship Bali resorts to eliminate all waste. The result? Creative initiatives that inspire colleagues and guests to connect even further with their natural surroundings.</p>
+      <p>In Bahasa Indonesia, cinta Alam means “Love of Earth.” This concept is at the heart of everything at <a href="http://www.alilahotels.com/seminyak" class="content-link" target="_blank">Alila Seminyak</a>, where glass water bottles become sleek drinkware and discarded coconuts find new life as organic planters. As global leaders in sustainable tourism, Alila Hotels & Resorts launched the “Zero Waste to Landfill” project in August 2016, challenging its flagship Bali resorts to eliminate all waste. The result? Creative initiatives that inspire colleagues and guests to connect even further with their natural surroundings.</p>
     </section>
 
     <div class="bg-image--content bg-image--content--top-right">
@@ -27,7 +27,7 @@ include "template_parts/header.php" ?>
     <section class="content-section content-section--left">
       <h2>Caring For the Island of the Gods</h2>
       <p>Alila’s “Zero Waste to Landfill” project originated in response to the rising environmental threat from Bali’s landfill sites. Greenhouse gasses—20 times more potent than CO²—are impacting the farmland and watercourses used by the local communities. Instead of contributing to the problem, Alila hotels in Bali have stepped up to the plate, finding new ways to process waste into new, higher-value products and services.</p>
-      <p>Colleagues at <strong>Alila Seminyak</strong> take their environmental stewardship seriously—just ask Aras Alam, chief of engineering. “We are always learning, always taking on new projects and thinking of new ideas for making the property more sustainable,” he explains.</p>
+      <p>Colleagues at <a href="http://www.alilahotels.com/seminyak" class="content-link" target="_blank">Alila Seminyak</a> take their environmental stewardship seriously—just ask Aras Alam, chief of engineering. “We are always learning, always taking on new projects and thinking of new ideas for making the property more sustainable,” he explains.</p>
     </section>
 
     <div class="bg-image--content bg-image--content--top-left">
@@ -35,20 +35,20 @@ include "template_parts/header.php" ?>
     </div>
     <section class="content-section content-section--middle">
       <h2>Building Greener</h2>
-      <p>Walking into <strong>Alila Seminyak</strong>, one might not realize how thoughtfully attuned the building is to its natural surroundings. Built on a delectable slice of sandy beach, the luxury resort’s design weaves contemporary architecture into open, airy lounge spaces and lushly landscaped terraces. Beyond appealing aesthetics, Alila Seminyak’s design is grounded in sustainability and a celebration of its locale.</p>
+      <p>Walking into <a href="http://www.alilahotels.com/seminyak" class="content-link" target="_blank">Alila Seminyak</a>, one might not realize how thoughtfully attuned the building is to its natural surroundings. Built on a delectable slice of sandy beach, the luxury resort’s design weaves contemporary architecture into open, airy lounge spaces and lushly landscaped terraces. Beyond appealing aesthetics, Alila Seminyak’s design is grounded in sustainability and a celebration of its locale.</p>
       <p>The use of local building materials and the incorporation of environmental, energy and resource-efficient systems are also integral to the design. Interior finishes include certified recycled or reclaimed ulin wood, local Batu Jogia stone, and terrazzo flooring made by local, independent village craftsmen. An open-plan layout harnesses natural sea breezes to reduce energy use.</p>
   </section>
 
   <section class="content-section content-section--right">
     <h2>No If’s, And’s, or Wasted Coconuts</h2>
-    <p>Every product passing through <strong>Alila Seminyak</strong> is repurposed—even rainwater is filtered and transformed into freshwater. Leafy greens, peppers, and fragrant herbs are grown pesticide-free in the hotel’s on-site hydroponic water garden.</p>
-    <p>And who could forget the coconuts? No trip to Bali is complete without sipping a fresh kelapa muda (young coconut) by the beach, but they are difficult to discard—adding to the island’s ongoing challenges with waste. Instead, <strong>Alila Seminyak</strong> takes the finished coconuts from their outlets and reuses them as organic planters.</p>
+    <p>Every product passing through <a href="http://www.alilahotels.com/seminyak" class="content-link" target="_blank">Alila Seminyak</a> is repurposed—even rainwater is filtered and transformed into freshwater. Leafy greens, peppers, and fragrant herbs are grown pesticide-free in the hotel’s on-site hydroponic water garden.</p>
+    <p>And who could forget the coconuts? No trip to Bali is complete without sipping a fresh kelapa muda (young coconut) by the beach, but they are difficult to discard—adding to the island’s ongoing challenges with waste. Instead, <a href="http://www.alilahotels.com/seminyak" class="content-link" target="_blank">Alila Seminyak</a> takes the finished coconuts from their outlets and reuses them as organic planters.</p>
 </section>
 
 <section class="content-section content-section--middle">
   <h2>Naturally Sweet</h2>
-  <p>Can the zero-waste concept work behind the bar, too? Seasalt at <strong>Alila Seminyak</strong> rose to the challenge. Single-use plastic straws are long gone, replaced by straws made from natural sugarcane. The Stretched Pineapple cocktail, a fresh twist on a modern mojito, is garnished with pineapple candy made from dried, fermented pineapple meat. The award-winning mixology team also crafts their own specialty syrups, using every last bit of fruit juices, rinds, and seeds to sweeten each cocktail naturally. </p>
-  <p>Bali’s zero-waste initiative serves as an example that Alila properties worldwide hope to emulate. Alam says he hopes <strong>Alila Seminyak's</strong> efforts inspire their guests as well, reminding us that “every small change can make a big difference.”</p>
+  <p>Can the zero-waste concept work behind the bar, too? Seasalt at <a href="http://www.alilahotels.com/seminyak" class="content-link" target="_blank">Alila Seminyak</a> rose to the challenge. Single-use plastic straws are long gone, replaced by straws made from natural sugarcane. The Stretched Pineapple cocktail, a fresh twist on a modern mojito, is garnished with pineapple candy made from dried, fermented pineapple meat. The award-winning mixology team also crafts their own specialty syrups, using every last bit of fruit juices, rinds, and seeds to sweeten each cocktail naturally. </p>
+  <p>Bali’s zero-waste initiative serves as an example that Alila properties worldwide hope to emulate. Alam says he hopes <a href="http://www.alilahotels.com/seminyak" class="content-link" target="_blank">Alila Seminyak's</a> efforts inspire their guests as well, reminding us that “every small change can make a big difference.”</p>
 </section>
   </div>
 </main>
